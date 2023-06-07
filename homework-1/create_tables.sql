bac78c07464542af763e8d45d7dc52dfb5275fc5
Mikhail Polyakov PyCharm <mikhapol@bk.ru>
2023-06-07 13:04:04 +0300

    HW-1 готово

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,27 +1,31 @@
 -- SQL-команды для создания таблиц
 
-CREATE TABLE employees
+CREATE TABLE employees  # Сотрудники
 (
-	employee_id int PRIMARY KEY,
-	first_name varchar(100) NOT NULL,
-	last_name varchar(100) NOT NULL,
-	title varchar(100),
-	birth_date date,
-	notes text
+	employee_id int PRIMARY KEY,                                    # Порядковый номер сотруника
+	first_name varchar(100) NOT NULL,                               # Имя
+	last_name varchar(100) NOT NULL,                                # Фамилия
+	title varchar(100),                                             # Должность
+	birth_date date,                                                # Дата рождения
+	notes text                                                      # О сотруднике и его достижениях
 );
 
-CREATE TABLE customers
+CREATE TABLE customers  # Клиенты
 (
-	customer_id varchar(100),
-	company_name varchar(100),
-	contact_name varchar(100)
+	customer_id varchar(5) PRIMARY KEY,                             # Идентификационное имя клиента
+	company_name varchar(50) NOT NULL,                              # Фирма (наименование компании)
+	contact_name varchar(50) NOT NULL                               # Имя представителя фирмы
 );
 
-CREATE TABLE orders
+CREATE TABLE orders     # Заказы
 (
-	order_id int PRIMARY KEY,
-	customer_id varchar(100),
-	employee_id int PRIMARY KEY,
-	order_date date,
-	ship_city varchar(100),
+	order_id int PRIMARY KEY,                                       # Идентификационный номер заказа
+	customer_id varchar(100) REFERENCES customers(customer_id),     # Идентификационное имя клиента
+	employee_id int REFERENCES employees(employee_id),              # Порядковый номер сотруника
+	order_date date NOT NULL,                                       # Дата заказа
+	ship_city varchar(100)                                          # Город заказа
 );
+
+SELECT * from employees
+SELECT * from customers
+SELECT * from orders
--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,56 @@
 """Скрипт для заполнения данными таблиц в БД Postgres."""
+import psycopg2
+import csv
+import os
+
+POSTGRES_API_KEY = os.getenv('POSTGRES_API_KEY')
+BASE_PATH = os.path.abspath("north_data")
+
+
+conn = psycopg2.connect(host="localhost", database="north", user="postgres", password=POSTGRES_API_KEY)
+try:
+    with conn:
+        with conn.cursor() as cur:
+
+            # Занесение информации в таблицу "employees"
+            with open(os.path.join(BASE_PATH, 'employees_data.csv'), 'r', encoding='utf-8') as csv_file:
+                reader = csv.DictReader(csv_file)
+                for item in reader:
+                    cur.execute('insert into employees values (%s, %s, %s, %s, %s,%s)',
+                                (
+                                    item['employee_id'],
+                                    item['first_name'],
+                                    item['last_name'],
+                                    item['title'],
+                                    item['birth_date'],
+                                    item['notes']
+                                ))
+
+            # Занесение информации в таблицу "customers"
+            with open(os.path.join(BASE_PATH, 'customers_data.csv'), 'r', encoding='utf-8') as csv_file:
+                reader = csv.DictReader(csv_file)
+                for item in reader:
+                    cur.execute('insert into customers values (%s, %s, %s)',
+                                (
+                                    item['customer_id'],
+                                    item['company_name'],
+                                    item['contact_name']
+                                ))
+
+            # Занесение информации в таблицу "orders"
+            with open(os.path.join(BASE_PATH, 'orders_data.csv'), 'r', encoding='utf-8') as csv_file:
+                reader = csv.DictReader(csv_file)
+                for item in reader:
+                    cur.execute('insert into orders values (%s, %s, %s, %s, %s)',
+                                (
+                                    item['order_id'],
+                                    item['customer_id'],
+                                    item['employee_id'],
+                                    item['order_date'],
+                                    item['ship_city']
+                                ))
+
+finally:
+    conn.close()
+
+
