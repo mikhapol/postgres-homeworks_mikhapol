57bea8441f5387f2daf50aeeb065c3fc81cdab54
Mikhail Polyakov PyCharm <mikhapol@bk.ru>
2023-06-08 20:22:54 +0300

    HW-2 simple_queries.sql 1 из 3 выполнено

--- homework-2/simple_queries.sql
+++ homework-2/simple_queries.sql
@@ -1,14 +1,26 @@
+SELECT * from customers
+SELECT * from orders
+
 -- Напишите запросы, которые выводят следующую информацию:
 -- 1. "имя контакта" и "город" (contact_name, country) из таблицы customers (только эти две колонки)
-SELECT ...
+SELECT contact_name, country
+FROM customers
 
 -- 2. идентификатор заказа и разницу между датами формирования (order_date) заказа и его отгрузкой (shipped_date) из таблицы orders
-
+SELECT order_id,
+		order_date,
+		shipped_date,
+		shipped_date - order_date as difference_date
+FROM orders
 
 -- 3. все города без повторов, в которых зарегистрированы заказчики (customers)
-
+SELECT DISTINCT city
+FROM customers
 
 -- 4. количество заказов (таблица orders)
-
+SELECT COUNT(*) as count_orders
+FROM orders
 
 -- 5. количество стран, в которые отгружался товар (таблица orders, колонка ship_country)
+SELECT COUNT(DISTINCT ship_country) as count_city_orders
+FROM orders
